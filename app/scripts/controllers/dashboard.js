0193e4b3714ed307c9fd183f7365fc2cfaebcc90
Nuno Carvalho <mestre.smash@gmail.com>
2015-10-04 16:03:44 +0100

    Fix graphs

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -11,39 +11,8 @@ angular.module('druidWebApp')
 
     $scope.queues = {};
     $scope.charts = {};
-
-    $scope.weekdays = new Array('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat');
-    var days = 4;
-    var now = new Date();
-    var mydays = [];
-    while ( days >= 0) {
-        var ago = new Date();
-        ago.setDate(ago.getDate()-days);
-        mydays.push($scope.weekdays[ago.getDay()]);
-        days--;
-    }
-    $scope.weekdays = mydays;
-    console.log(mydays);
-
-    $scope.options = {
-            chart: {
-                type: 'lineChart',
-                height: 150,
-                x: function(d){ return d.x; },
-                y: function(d){ return d.y; },
-                useInteractiveGuideline: true,
-                xAxis: {
-                    axisLabel: 'Last 5 days',
-                    tickValues: [0,1,2,3,4],
-                    tickFormat: function(d) { return $scope.weekdays[d]; }
-                },
-                yAxis: {
-                  tickValues: [0,5,10]
-                },
-                yDomain: [0,10]
-            }
-        };
-
+    $scope.options = {};
+    $scope.labels = undefined;
 
     $http
       .get(API+'/queues')
@@ -56,11 +25,33 @@ angular.module('druidWebApp')
               .get(API+'/chart1/'+curr._id)
               .then( function(response) {
                 if (response.status === 200 ) {
+                  console.log(response.data);
                   var myData = [
                     { key: 'todo', values: response.data.todo, color: 'red'},
                     { key: 'done', values: response.data.done, color: 'green'}
                   ];
-                  console.log(myData);
+                  if ($scope.labels === undefined) {
+                    var mylabels = new Array();
+                    for (var j = 0 ; j < response.data.todo.length ; j++ ) {
+                      mylabels.push(response.data.todo[j].label);
+                    }
+                    $scope.labels = mylabels;
+                  }
+                  $scope.options[response.data.queue] = {
+                    chart: {
+                        type: 'lineChart',
+                        height: 150,
+                        x: function(d){ return d.x; },
+                        y: function(d){ return d.y; },
+                        useInteractiveGuideline: true,
+                        xAxis: {
+                            axisLabel: 'Last 5 days',
+                            tickValues: [0,1,2,3,4],
+                            tickFormat: function(d) { return $scope.labels[d]; }
+                        },
+                        yDomain: [0, response.data.max]
+                    }
+                  };
                   $scope.charts[response.data.queue] = myData;
                 }
               });
